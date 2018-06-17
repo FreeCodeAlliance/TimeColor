a3f250fc68bb554fe3d3c69b1fb47a506e38fcab
xiejicangqiong <1069194022@qq.com>
2018-06-17 11:10:44 +0800

    跨域

--- server/app.js
+++ server/app.js
@@ -13,6 +13,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    if(req.method=="OPTIONS")
+      res.send(200);
+    else
+      next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
