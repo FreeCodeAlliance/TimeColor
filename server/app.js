daf9f533ccbff58545dad8054a1cfd380c69bb16
xiejicangqiong <1069194022@qq.com>
2018-06-17 12:56:46 +0800

    test json

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json;charset=utf-8");
     if(req.method=="OPTIONS")
       res.send(200);
     else
