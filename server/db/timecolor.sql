418f860bad1e1eb9e3df7d904302b82769c4d97f
1xx2xx3 <1069194022@qq.com>
2018-07-07 18:49:53 +0800

    数据库sql文件

---
+++ server/db/timecolor.sql
@@ -0,0 +1,139 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mlqaliy
+Source Server Version : 50718
+Source Host           : rm-uf6o6pl97i35t27emxo.mysql.rds.aliyuncs.com:3306
+Source Database       : timecolor
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2018-07-07 18:45:08
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `bet`
+-- ----------------------------
+DROP TABLE IF EXISTS `bet`;
+CREATE TABLE `bet` (
+  `issue` varchar(12) NOT NULL,
+  `uid` smallint(10) NOT NULL,
+  `tth` text NOT NULL COMMENT '万',
+  `tho` text NOT NULL COMMENT '千',
+  `hun` text NOT NULL COMMENT '百',
+  `ten` text NOT NULL COMMENT '百',
+  `ind` text NOT NULL COMMENT '个位',
+  `big` int(20) unsigned zerofill NOT NULL COMMENT '大',
+  `small` int(20) unsigned zerofill NOT NULL COMMENT ' 小',
+  `single` int(20) unsigned zerofill NOT NULL,
+  `even` int(20) unsigned zerofill NOT NULL,
+  `gain` int(20) NOT NULL DEFAULT '-1' COMMENT '赢取多少金额',
+  PRIMARY KEY (`issue`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of bet
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `lottery`
+-- ----------------------------
+DROP TABLE IF EXISTS `lottery`;
+CREATE TABLE `lottery` (
+  `issue` varchar(12) NOT NULL,
+  `result` varchar(5) NOT NULL COMMENT '万千百十个',
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`issue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of lottery
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `masterinfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `masterinfo`;
+CREATE TABLE `masterinfo` (
+  `uid` smallint(10) NOT NULL AUTO_INCREMENT,
+  `account` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`uid`,`account`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of masterinfo
+-- ----------------------------
+INSERT INTO `masterinfo` VALUES ('1', 'master', 'e10adc3949ba59abbe56e057f20f883e', '2018-06-23 21:39:15');
+
+-- ----------------------------
+-- Table structure for `modifylottery`
+-- ----------------------------
+DROP TABLE IF EXISTS `modifylottery`;
+CREATE TABLE `modifylottery` (
+  `masterid` smallint(10) NOT NULL,
+  `issue` varchar(12) NOT NULL,
+  `date` datetime NOT NULL,
+  `modify` varchar(5) NOT NULL COMMENT '万千百十个',
+  PRIMARY KEY (`masterid`,`issue`,`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of modifylottery
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `recharge`
+-- ----------------------------
+DROP TABLE IF EXISTS `recharge`;
+CREATE TABLE `recharge` (
+  `userid` smallint(10) NOT NULL,
+  `value` int(20) NOT NULL,
+  `date` datetime NOT NULL,
+  `masterid` smallint(10) NOT NULL,
+  PRIMARY KEY (`userid`,`value`,`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of recharge
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `register`
+-- ----------------------------
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE `register` (
+  `account` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `disktype` varchar(3) NOT NULL DEFAULT 'D',
+  `date` datetime NOT NULL,
+  `remark` text,
+  PRIMARY KEY (`account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of register
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `userinfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `userinfo`;
+CREATE TABLE `userinfo` (
+  `uid` smallint(10) NOT NULL AUTO_INCREMENT COMMENT '用户id 自动增加',
+  `account` varchar(20) NOT NULL COMMENT '账号',
+  `password` varchar(32) NOT NULL COMMENT '密码',
+  `disktype` varchar(3) NOT NULL DEFAULT 'D' COMMENT '盘类',
+  `quota` int(20) unsigned zerofill NOT NULL DEFAULT '00000000000000000000' COMMENT '额度',
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`uid`,`account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of userinfo
+-- ----------------------------
