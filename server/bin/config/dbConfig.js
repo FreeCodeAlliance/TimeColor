e820aa5dff65e02a736c1b2691c16f9393d8c41d
shenl <673952382@qq.com>
2019-04-16 14:57:46 +0800

    服务器正常配置

--- server/bin/config/dbConfig.js
+++ server/bin/config/dbConfig.js
@@ -1,21 +1,33 @@
 // 数据库配置
 
-// 本地配置
+//47服务器本地配置
 tc.sqlconfig = {
    // host:'rm-uf6o6pl97i35t27emxo.mysql.rds.aliyuncs.com',           // 主机
+    host: '127.0.0.1',
+    user:'shenl',                                                    // 用户名
+    password:'923',                                            // 密码
+    //database:'shenltestdb',                                           // 数据库名
+    database:'timecolor',                                           // 数据库名
+    port:'3306',                                                    // 端口号
+};
+
+//服务器配置
+// tc.sqlconfig = {
+//     host:'47.106.215.192',             // 主机
+//     user:'shenl',                                                    // 用户名
+//     password:'923',                                            // 密码
+//     database:'timecolor',                                           // 数据库名
+//     port:'3306',                                                    // 端口号
+// };
+
+
+//window本地配置
+/*
+tc.sqlconfig = {
     host: '127.0.0.1',
     user:'root',                                                    // 用户名
     password:'',                                            // 密码
     database:'shenltestdb',                                           // 数据库名
-    //database:'timecolor',                                           // 数据库名
     port:'3307',                                                    // 端口号
 };
-
-// 服务器配置
-// tc.sqlconfig = {
-//     host:'rm-uf6o6pl97i35t27em.mysql.rds.aliyuncs.com',             // 主机
-//     user:'root',                                                    // 用户名
-//     password:'M123456a',                                            // 密码
-//     database:'timecolor',                                           // 数据库名
-//     port:'3306',                                                    // 端口号
-// };
+*/
