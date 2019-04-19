c51da66fbb4834973e6cd0a1e4de347946919040
shenl <673952382@qq.com>
2019-04-19 16:46:31 +0800

    优化  操作 用户的界面

--- client/src/scene/master/users-gifts/controlUserDialog/index.js
+++ client/src/scene/master/users-gifts/controlUserDialog/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { InputNumber, Modal, Radio, Button, message, Input, Icon } from 'antd';
+import { InputNumber, Modal, Radio, Button, message, Input, Icon, Tabs } from 'antd';
 import { connect } from "react-redux";
 import './index.less'
 import { giveGiftUser, userSignIn } from "../../../../actions/gift"
@@ -8,6 +8,7 @@ import {GIFT_DESC} from "../../../../constant/gift";
 
 const RadioGroup = Radio.Group;
 const giftsDesc = GIFT_DESC;
+const TabPane = Tabs.TabPane;
 
 class ControlUserDialog extends PureComponent {
     static propTypes = {
@@ -77,13 +78,55 @@ class ControlUserDialog extends PureComponent {
         const { onUserUpdateName, userInfo } = this.props;
         const { userUpdateName } = this.state;
         if (onUserUpdateName) { onUserUpdateName(userInfo.uid, userUpdateName); }
-    }
+    };
     onChangeUserName = (e) => {
         this.setState({ userUpdateName: e.target.value });
+    };
+    renderRecordGift = () => {
+        return (
+            <div className='userControlCell'>
+                <RadioGroup onChange={this.onGiftSelectChange} value={this.state.giftIndex}>
+                    <Radio value={0} className="gift-level1">{GIFT_DESC[0]}</Radio>
+                    <Radio value={1} className="gift-level2">{GIFT_DESC[1]}</Radio>
+                    <Radio value={2} className="gift-level3">{GIFT_DESC[2]}</Radio>
+                    <Radio value={3} className="gift-level1">{GIFT_DESC[3]}</Radio>
+                    <Radio value={4} className="gift-level2">{GIFT_DESC[4]}</Radio>
+                    <Radio value={5} className="gift-level3">{GIFT_DESC[5]}</Radio>
+                </RadioGroup>
+                <Button className='userControlCell-button' type="primary" onClick={this.onGiveGift}>记礼包</Button>
+            </div>
+        );
+    };
+    renderFightTime = () => {
+        return (
+            <div className='userFightTime'>
+                <p>本周次数</p>
+                <InputNumber min={-4} max={4} defaultValue={this.state.fightTime} onChange={this.onTimesChange} />
+                <Button type="primary" onClick={this.onUserSign}>参战签到</Button>
+            </div>
+        )
+    };
+
+    renderUpateName = () => {
+        const { userUpdateName } = this.state;
+        return (
+            <div className='userControlUpdateName'>
+                <Input
+                style={{width: "180px"}}
+                size="default"
+                placeholder="修改玩家姓名"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                value={userUpdateName}
+                onChange={this.onChangeUserName}
+                ref={node => this.userNameInput = node}
+                />
+                <Button type="primary" onClick={this.onUserUpdateName} disabled={userUpdateName.length <= 0} >玩家改名</Button>
+        </div>
+        )
     }
     render() {
         const { visible, onCancel, userInfo } = this.props;
-        const { userUpdateName } = this.state;
+
         return (
             <Modal
                 title={userInfo.name}
@@ -93,39 +136,25 @@ class ControlUserDialog extends PureComponent {
                 maskClosable
                 onCancel={onCancel}
             >
-                <div className='userControlCell'>
-                    <RadioGroup onChange={this.onGiftSelectChange} value={this.state.giftIndex}>
-                        <Radio value={0} className="gift-level1">{GIFT_DESC[0]}</Radio>
-                        <Radio value={1} className="gift-level2">{GIFT_DESC[1]}</Radio>
-                        <Radio value={2} className="gift-level3">{GIFT_DESC[2]}</Radio>
-                        <Radio value={3} className="gift-level1">{GIFT_DESC[3]}</Radio>
-                        <Radio value={4} className="gift-level2">{GIFT_DESC[4]}</Radio>
-                        <Radio value={5} className="gift-level3">{GIFT_DESC[5]}</Radio>
-                    </RadioGroup>
-                    <Button type="primary" onClick={this.onGiveGift}>记礼包</Button>
-                </div>
-                <div className='divLine' />
-                <div className='userControlCell'>
-                    <p>本周次数</p>
-                    <InputNumber min={-4} max={4} defaultValue={this.state.fightTime} onChange={this.onTimesChange} />
-                    <Button type="primary" onClick={this.onUserSign}>参战签到</Button>
-                </div>
-                <div className='divLine' />
-                <div className='userControlCell'>
-                    <Input
-                        style={{width: "180px"}}
-                        size="default"
-                        placeholder="修改玩家姓名"
-                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                        value={userUpdateName}
-                        onChange={this.onChangeUserName}
-                        ref={node => this.userNameInput = node}
-                    />
-                    <Button type="primary" onClick={this.onUserUpdateName} disabled={userUpdateName.length <= 0} >玩家改名</Button>
-                </div>
-                <div className="dialog-footer">
-                    <Button type="danger" onClick={this.onDelete}>删除用户及其礼包数据</Button>
-                </div>
+                <Tabs
+                    defaultActiveKey="1"
+                    tabPosition="left"
+                    style={{ height: 220 }}
+                >
+                    <TabPane tab="发放礼包" key="1">
+                        {this.renderRecordGift()}
+                    </TabPane>
+                    <TabPane tab="打架签到" key="2">
+                        {this.renderFightTime()}
+                    </TabPane>
+                    <TabPane tab="玩家名称" key="3">
+                        {this.renderUpateName()}
+                    </TabPane>
+                    <TabPane tab="删除用户" key="4">
+                        <Button type="danger" onClick={this.onDelete}>删除用户及其礼包数据</Button>
+                    </TabPane>
+                </Tabs>
+
             </Modal>
         )
     }
